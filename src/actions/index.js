fdadb7db52531a31dd7903e22832116e62f1d1f6
VIVIANA MARCELO <arivichi16@gmail.com>
2018-03-13 16:50:13 -0300

    Updates

--- src/actions/index.js
+++
@@ -1,6 +0,0 @@
-export const SET_CITY = 'SET_CITY'
-
-export const setCity = value => ({
-        type: SET_CITY,
-        value
-})
--- src/constant/wathers.js
+++
@@ -1,8 +0,0 @@
-export const CLOUDY = 'cloudy';
-export const SUN = 'day-sunny';
-export const CLOUD = 'cloud';
-export const RAIN = 'rain';
-export const SNOW = 'snow';
-export const WINDY = 'windy';
-export const THUNDER = 'thunder';
-export const DRIZZLE = 'drizzle';
